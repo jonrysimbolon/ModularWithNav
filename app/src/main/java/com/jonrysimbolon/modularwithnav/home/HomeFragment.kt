c0ea7c7000d949c8be6dfa14e9d2cd0cdc665100
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-07-22 14:34:36 +0700

    Initial commit

---
+++ app/src/main/java/com/jonrysimbolon/modularwithnav/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.jonrysimbolon.modularwithnav.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonrysimbolon.modularwithnav.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+    }
+}
